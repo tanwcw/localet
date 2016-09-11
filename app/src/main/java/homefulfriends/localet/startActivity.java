75e8c032736d08ff5e0c4a940096c342c060b5e5
Woan Chin <woanchin.tee@gmail.com>
2016-09-11 00:41:58 -0700

    PLEASE PULL THIS FILE.

--- app/src/main/java/homefulfriends/localet/startActivity.java
+++ app/src/main/java/homefulfriends/localet/startActivity.java
@@ -6,7 +6,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class StartActivity extends Activity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
