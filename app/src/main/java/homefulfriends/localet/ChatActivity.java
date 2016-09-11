65da2ff494221ae3ef45a7e1de48c0fb117befc6
tanwcw <williamtan@Williams-MacBook-Air.local>
2016-09-11 14:06:50 -0700

    fix chatbox

--- app/src/main/java/homefulfriends/localet/ChatActivity.java
+++ app/src/main/java/homefulfriends/localet/ChatActivity.java
@@ -1,9 +1,11 @@
 package homefulfriends.localet;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -12,6 +14,14 @@ import android.widget.TextView;
 import com.google.android.gms.appindexing.Action;
 import com.google.android.gms.appindexing.AppIndex;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.ibm.watson.developer_cloud.conversation.v1.ConversationService;
+import com.ibm.watson.developer_cloud.conversation.v1.model.MessageRequest;
+import com.ibm.watson.developer_cloud.conversation.v1.model.MessageRequest.Builder;
+import com.ibm.watson.developer_cloud.conversation.v1.model.MessageResponse;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class ChatActivity extends Activity {
     TextView resultView;
@@ -66,65 +76,67 @@ public class ChatActivity extends Activity {
 
         @Override
         protected String doInBackground(String... input) {
-            System.out.println("hallo world");
-//            final ConversationService service = new ConversationService("2016-07-11");
-//            service.setUsernameAndPassword("486ba223-ffe4-471d-9298-6077153070a8", "doDFV1RkJB7t");
-//
-//            MessageRequest newMessage = new Builder()
-//                    .inputText(input[0])
-//                    // Replace with the context obtained from the initial request
-//                    //.context(...)
-//                    .build();
-//
-//            String workspaceId = "48a2b3f0-63e4-4dd5-828f-f91ff525b75f";
-//
-//            MessageResponse response = service
-//                    .message(workspaceId, newMessage)
-//                    .execute();
-//
-//            //String str = response.toString();
-//            //str = str.replaceAll("[", "{");
-//            //str = str.replaceAll("]", "}");
-//            //Log.e("response: ", String.valueOf(response));
-//            //Log.e("response: ",(JSONObject)(response.toString()));
-//            //Log.e("response: ", String.valueOf(response));
-//            //Log.e("response: ", String.valueOf(response));
-//
-//            try {
-//                Log.e("response: ", response.toString());
-//                JSONObject result = new JSONObject(response.toString());
-//                JSONArray intents = result.getJSONArray("intents");
-//                JSONArray entities = result.getJSONArray("entities");
-//                String intent = "";
-//                String value = "";
-//
-//                for (int i = 0; i < intents.length(); i++) {
-//                    intent = intents.getJSONObject(i).getString("intent");
-//                }
-//                for (int i = 0; i < entities.length(); i++) {
-//                    value = entities.getJSONObject(i).getString("value");
-//                }
-//
-//                if (intent.equals("others")) {
-//                    //call up mapactivity
-//                    Intent i = new Intent(getApplicationContext(), MapActivity.class);
-//                    startActivity(i);
-//                } else {
-//                    if (intent.equals("review")) {
-//                        //yelp
-//                        String[] lol = {};
-//                        res = YelpAPI.main(lol);
-//
-//                    } else {
-//                        //history
-//                        String[] lol = {"Golden_Gate_Bridge", "history" };
-//                        res = WikiScraper.main(lol);
-//                    }
-//                }
-//            } catch (JSONException e) {
-//                e.printStackTrace();
-//                return "Error";
-//            }
+            System.out.println(input.toString()+"HEREEEEEEEE");
+            final ConversationService service = new ConversationService("2016-07-11");
+            service.setUsernameAndPassword("486ba223-ffe4-471d-9298-6077153070a8", "doDFV1RkJB7t");
+
+            MessageRequest newMessage = new Builder()
+                    .inputText(input[0])
+                    // Replace with the context obtained from the initial request
+                    //.context(...)
+                    .build();
+            System.out.println(newMessage.toString());
+            String workspaceId = "48a2b3f0-63e4-4dd5-828f-f91ff525b75f";
+
+            MessageResponse response = service
+                    .message(workspaceId, newMessage)
+                    .execute();
+
+            //String str = response.toString();
+            //str = str.replaceAll("[", "{");
+            //str = str.replaceAll("]", "}");
+            //Log.e("response: ", String.valueOf(response));
+            //Log.e("response: ",(JSONObject)(response.toString()));
+            //Log.e("response: ", String.valueOf(response));
+            //Log.e("response: ", String.valueOf(response));
+            System.out.println("=========================================================lol");
+
+            try {
+                Log.e("response: ", response.toString());
+                JSONObject result = new JSONObject(response.toString());
+                JSONArray intents = result.getJSONArray("intents");
+                JSONArray entities = result.getJSONArray("entities");
+                String intent = "";
+                String value = "";
+
+                for (int i = 0; i < intents.length(); i++) {
+                    intent = intents.getJSONObject(i).getString("intent");
+                }
+                for (int i = 0; i < entities.length(); i++) {
+                    value = entities.getJSONObject(i).getString("value");
+                }
+                System.out.println(intent);
+
+                if (intent.equals("others")) {
+                    //call up mapactivity
+                    Intent i = new Intent(getApplicationContext(), MapActivity.class);
+                    startActivity(i);
+                } else {
+                    if (intent.equals("review")) {
+                        //yelp
+                        String[] lol = {};
+                        res = YelpAPI.main(lol);
+
+                    } else {
+                        //history
+                        String[] lol = {"Golden_Gate_Bridge", "history" };
+                        res = WikiScraper.main(lol);
+                    }
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+                return "Error";
+            }
             return "";
         }
 
@@ -132,9 +144,9 @@ public class ChatActivity extends Activity {
         protected void onPostExecute(String result) {
             TextView showText = (TextView) findViewById(R.id.textView);
             resultView.setText(res);
-            showText.setText(result);
         }
     }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -142,11 +154,6 @@ public class ChatActivity extends Activity {
         setContentView(R.layout.activity_chat);
         // ATTENTION: This was auto-generated to implement the App Indexing API.
         // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
-        resultView = (TextView) findViewById(R.id.result);
-    }
-
-    public void onClick(View v) {
         final ImageView button = (ImageView) findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -155,5 +162,8 @@ public class ChatActivity extends Activity {
                 new IBM().execute(input);
             }
         });
+
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+        resultView = (TextView) findViewById(R.id.result);
     }
 }
